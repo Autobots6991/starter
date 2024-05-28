55140b17114d330a560cb16e60fe30f0cb6269e1
Autobots6991 <94107407+Autobots6991@users.noreply.github.com>
2024-05-28 14:08:05 +0700

    Update api.get

--- script.js
+++ script.js
@@ -5,29 +5,23 @@ const api = express();
 const HOST = "localhost";
 const PORT = 88;
 api.listen(PORT, () => console.log(`Server Running at port ${PORT}!`));
-const APIkey = "5506b39b271a1511c0ede0116acf841f";
-let cityname = "Hanoi";
-const unit = "metric";
-axios
-  .get(
-    `https://api.openweathermap.org/data/2.5/weather?q=${cityname}&appid=${APIkey}&units=${unit}`
-  )
-  .then((response) =>
-    fs.writeFileSync(
-      "./weatherdata.json",
-      JSON.stringify(response.data, null, 2),
-      (err) => {
-        if (err) throw err;
-        else {
-          console.log("The file is updated with the given data");
-        }
-      }
-    )
-  );
+
+let wedata;
+
 api.get("/", (req, res) => {
-  res.send("Welcome to weather data API dit me may Nhan!");
+  res.send("Welcome to weather data API dit me may Nhana!");
 });
-const wedata = require("./weatherdata.json");
 api.get("/weather", (req, res) => {
+  const APIkey = "5506b39b271a1511c0ede0116acf841f";
+  let cityname = "London";
+  const unit = "metric";
+  axios
+    .get(
+      `https://api.openweathermap.org/data/2.5/weather?q=${cityname}&appid=${APIkey}&units=${unit}`
+    )
+    .then((response) => {
+      wedata = response.data;
+      return wedata;
+    });
   res.send(wedata);
 });
